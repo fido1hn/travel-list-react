be47bf5034bd784135d9782d2f32c1d335a317d9
fido1hn <confidoekeanya@gmail.com>
2025-04-08 03:25:34 +0100

    rendering the list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -12,6 +18,7 @@ function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,9 +26,30 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
